7b1c238a346eaf2b012841dfceffb49723a86721
riteshrts2k4757-star <riteshrts2k4757@gmail.com>
2026-09-23 01:50:10 +0530

    Fix React Navigation instant crash by adding SafeAreaProvider

--- mobile/mobile/App.tsx
+++ mobile/mobile/App.tsx
@@ -1,12 +1,15 @@
 import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
+import { SafeAreaProvider } from 'react-native-safe-area-context';
 import AppNavigator from './src/navigation/AppNavigator';
 
 function App(): React.JSX.Element {
   return (
-    <NavigationContainer>
-      <AppNavigator />
-    </NavigationContainer>
+    <SafeAreaProvider>
+      <NavigationContainer>
+        <AppNavigator />
+      </NavigationContainer>
+    </SafeAreaProvider>
   );
 }
 
