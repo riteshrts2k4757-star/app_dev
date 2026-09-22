11ca28a44ea8515d718658f4d109aaf69f2ca2db
riteshrts2k4757-star <riteshrts2k4757@gmail.com>
2026-09-23 01:51:09 +0530

    Import SyncService to start MQTT listener

--- mobile/mobile/App.tsx
+++ mobile/mobile/App.tsx
@@ -1,9 +1,15 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
 import AppNavigator from './src/navigation/AppNavigator';
+import { startSyncManager } from './src/services/SyncService';
 
 function App(): React.JSX.Element {
+  useEffect(() => {
+    // Start background sync manager on app launch
+    startSyncManager();
+  }, []);
+
   return (
     <SafeAreaProvider>
       <NavigationContainer>
